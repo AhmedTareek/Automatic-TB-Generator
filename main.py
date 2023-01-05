fae52573279f2b768ffae00dfa22a78d1be6fc59
KareemWael1 <87460487+KareemWael1@users.noreply.github.com>
2023-01-05 20:17:26 +0200

    executable uploaded

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from VerilogGenerator import Generator
 
 
 def main():
-    filepath = input("Enter The path of the .v file: ")
-    p1 = Parser(filepath)
-    index = filepath.rfind('\\') + 1
-    filepath = filepath[:index]
-    mod = p1.modules
-    for module in mod:
-        print(module)
-        m1 = Module(module)
-        mod_name = m1.name
-        ports = m1.variables
-        Generator(mod_name, ports, filepath)
+    while 1:
+        filepath = input("Enter The path of the .v file: ")
+        p1 = Parser(filepath)
+        index = filepath.rfind('\\') + 1
+        filepath = filepath[:index]
+        mod = p1.modules
+        for module in mod:
+            print(module)
+            m1 = Module(module)
+            mod_name = m1.name
+            ports = m1.variables
+            Generator(mod_name, ports, filepath)
 
 
 # Press the green button in the gutter to run the script.
